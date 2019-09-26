ab32f712ae8556e7575a9218d62a1e2d1134641a
169000707zhang <447613512@qq.com>
2019-09-26 17:48:39 +0800

    2019

---
+++ evaluation/src/main/java/evaluation/entity/Teach_v.java
@@ -0,0 +1,26 @@
+package evaluation.entity;
+
+public class Teach_v{
+	private int teacherid;
+	public int getTeacherid() {
+		return teacherid;
+	}
+	public void setTeacherid(int teacherid) {
+		this.teacherid = teacherid;
+	}
+	public String getMajorname() {
+		return majorname;
+	}
+	public void setMajorname(String majorname) {
+		this.majorname = majorname;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	private String majorname;
+	private String name;
+	
+}
